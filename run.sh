9468c02d081f29601b8277fe28d35a260f2b7eef
kondo takeshi <chaspy@users.noreply.github.com>
2023-04-01 14:09:36 +0900

    count

--- run.sh
+++ run.sh
@@ -4,8 +4,9 @@ BASEURL="https://app.magicpod.com/api"
 # ORGANIZATION_NAME=$ORGANIZATION_NAME
 # PROJECT_NAME=$PROJECT_NAME
 BATCH_RUNS_PATH="/v1.0/${ORGANIZATION_NAME}/${PROJECT_NAME}/batch-runs/"
+COUNT="100"
 
-curl -X 'GET' \
-  "${BASEURL}${BATCH_RUNS_PATH}?count=20" \
+curl -s -X 'GET' \
+  "${BASEURL}${BATCH_RUNS_PATH}?count=${COUNT}" \
   -H 'accept: application/json' \
   -H "Authorization: Token $MAGICPOD_API_TOKEN"
