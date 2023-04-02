ba956948ef10f4d63dea3b757cedfb8a6ea5e24d
kondo takeshi <chaspy@users.noreply.github.com>
2023-04-02 16:16:04 +0900

    request

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import {run} from './run'
 const main = async (): Promise<void> => {
   await run({
     dd_api_key: core.getInput('dd_api_key', {required: true}),
-    magicpod_api_key: core.getInput('magicpod_api_key', {required: true})
+    magicpod_api_key: core.getInput('magicpod_api_key', {required: true}),
+    magicpod_organization_name: core.getInput('magicpod_organization_name', {
+      required: true
+    }),
+    magicpod_project_name: core.getInput('magicpod_project_name', {
+      required: true
+    })
   })
 }
 
--- src/run.ts
+++ src/run.ts
@@ -1,8 +1,11 @@
 import * as core from '@actions/core'
+import axios from 'axios'
 
 type Inputs = {
   dd_api_key: string
   magicpod_api_key: string
+  magicpod_organization_name: string
+  magicpod_project_name: string
 }
 
 // eslint-disable-next-line @typescript-eslint/require-await
@@ -12,8 +15,17 @@ export const run = async (inputs: Inputs): Promise<void> => {
   // load insputs
   const dd_api_key = inputs.dd_api_key
   const magicpod_api_key = inputs.magicpod_api_key
+  const magicpod_organization_name = inputs.magicpod_organization_name
+  const magicpod_project_name = inputs.magicpod_project_name
+  const count = 100
 
   // Get response from magicpod
+  sendHttpRequest(
+    magicpod_api_key,
+    magicpod_organization_name,
+    magicpod_project_name,
+    count
+  )
 
   // parse response
 
@@ -22,4 +34,24 @@ export const run = async (inputs: Inputs): Promise<void> => {
   // build metrics for datadog
 
   // send metric to datadog
+}
+
+async function sendHttpRequest(
+  magicpod_api_key: string,
+  magicpod_organization_name: string,
+  magicpod_project_name: string,
+  count: number
+) {
+  const url = `https://app.magicpod.com/api/v1.0/${magicpod_organization_name}/${magicpod_project_name}/batch-runs/?count=${count}`
+  const headers = {
+    accept: 'application/json',
+    Authorization: `Token ${magicpod_api_key}`
+  }
+
+  try {
+    const response = await axios.get(url, {headers})
+    console.log(response.data)
+  } catch (error) {
+    console.error(`Error: ${error}`)
+  }
 }
