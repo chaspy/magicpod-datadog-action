4c3eae1d8586656cae5fc65e4b92ee6006dbaaf5
kondo takeshi <chaspy@users.noreply.github.com>
2023-04-02 11:43:54 +0900

    say hello

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,8 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
+import * as core from '@actions/core'
 
 async function run() {
-  try {
-    const message = core.getInput('message');
-    console.log(message);
-
-    const token = core.getInput('token');
-    const context = github.context;
-    const octokit = github.getOctokit(token);
-
-    const newIssue = await octokit.issues.create({
-      ...context.repo,
-      title: 'My GitHub Action',
-      body: `Hello, I am a GitHub Action created with TypeScript! Message: ${message}`,
-    });
-
-    core.setOutput('issue', JSON.stringify(newIssue.data));
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+  const message = core.getInput('message')
+  console.log(message)
 }
 
-run();
-
+run()
