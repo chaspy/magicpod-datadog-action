67ffad3406a1b4495f431b0a627b9ae91729e488
kondo takeshi <chaspy@users.noreply.github.com>
2023-04-02 11:56:24 +0900

    run

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import * as core from '@actions/core'
+// import * as core from '@actions/core'
 
 async function run() {
-  const message = core.getInput('message')
+  //  const message = core.getInput('message')
+  const message = 'hello'
   console.log(message)
 }
 
