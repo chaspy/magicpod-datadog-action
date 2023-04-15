b356ce263bf2bad6096425244d6bf6dceaea222a
kondo takeshi <chaspy@users.noreply.github.com>
2023-04-15 13:15:23 +0900

    compile

--- dist/datadog.js
+++ dist/datadog.js
@@ -3,13 +3,13 @@
  * Submit metrics returns "Payload accepted" response
  */
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.submitMetircs = void 0;
+exports.submitBatchRunMetircs = exports.submitBatchRunsMetircs = void 0;
 // https://docs.datadoghq.com/ja/api/latest/metrics/#submit-metrics
 const datadog_api_client_1 = require("@datadog/datadog-api-client");
 const magicpod_1 = require("./magicpod");
 const configuration = datadog_api_client_1.client.createConfiguration();
 const apiInstance = new datadog_api_client_1.v2.MetricsApi(configuration);
-function submitMetircs(timestamp, value, batch_run_number, test_setting_name, status, organization_name, project_name) {
+function submitBatchRunsMetircs(timestamp, value, batch_run_number, test_setting_name, status, organization_name, project_name) {
     const durationSecondParams = {
         body: {
             series: [
@@ -77,7 +77,82 @@ function submitMetircs(timestamp, value, batch_run_number, test_setting_name, st
         console.log(`timestamp ${timestamp} is not available. skip to send metrics`);
     }
 }
-exports.submitMetircs = submitMetircs;
+exports.submitBatchRunsMetircs = submitBatchRunsMetircs;
+function submitBatchRunMetircs(timestamp, value, batch_run_number, test_setting_name, status, organization_name, project_name, pattern_name, order, number) {
+    const durationSecondParams = {
+        body: {
+            series: [
+                {
+                    metric: 'custom.magicpod-datadog-action.test_case.duration_second',
+                    type: 3,
+                    points: [
+                        {
+                            timestamp: timestamp,
+                            value: value
+                        }
+                    ],
+                    tags: [
+                        `batch_run_number:${batch_run_number}`,
+                        `test_setting_name:${test_setting_name}`,
+                        `status:${status}`,
+                        `organization_name:${organization_name}`,
+                        `project_name:${project_name}`,
+                        `pattern_name:${pattern_name}`,
+                        `order:${order}`,
+                        `number:${number}`
+                    ],
+                    unit: 'Second'
+                }
+            ]
+        }
+    };
+    const countParams = {
+        body: {
+            series: [
+                {
+                    metric: 'custom.magicpod-datadog-action.test_case.count',
+                    type: 3,
+                    points: [
+                        {
+                            timestamp: timestamp,
+                            value: 1
+                        }
+                    ],
+                    tags: [
+                        `batch_run_number:${batch_run_number}`,
+                        `test_setting_name:${test_setting_name}`,
+                        `status:${status}`,
+                        `organization_name:${organization_name}`,
+                        `project_name:${project_name}`,
+                        `pattern_name:${pattern_name}`,
+                        `order:${order}`,
+                        `number:${number}`
+                    ],
+                    unit: 'Count'
+                }
+            ]
+        }
+    };
+    if (isTimestampAvailable(timestamp) && !(0, magicpod_1.isStatusRunning)(status)) {
+        console.log(`info: timestamp: ${timestamp}, project_name: ${project_name}, test_setting_name: ${test_setting_name}, status: ${status}, value: ${value}`);
+        apiInstance
+            .submitMetrics(durationSecondParams)
+            .then((data) => {
+            console.log('API called successfully. custom.magicpod-datadog-action.batch_run.duration_second is submitted.');
+        })
+            .catch((error) => console.error(error));
+        apiInstance
+            .submitMetrics(countParams)
+            .then((data) => {
+            console.log('API called successfully. custom.magicpod-datadog-action.batch_run.count is submitted.');
+        })
+            .catch((error) => console.error(error));
+    }
+    else {
+        console.log(`timestamp ${timestamp} is not available. skip to send metrics`);
+    }
+}
+exports.submitBatchRunMetircs = submitBatchRunMetircs;
 // https://docs.datadoghq.com/ja/api/latest/metrics/?code-lang=typescript#submit-metrics
 // points:
 // Timestamps should be in POSIX time in seconds,
--- dist/magicpod.js
+++ dist/magicpod.js
@@ -12,11 +12,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.isStatusRunning = exports.processBatchRunsData = exports.getBatchRuns = void 0;
+exports.getBatchRun = exports.isStatusRunning = exports.processBatchRunData = exports.processBatchRunsData = exports.getBatchRuns = void 0;
 const axios_1 = __importDefault(require("axios"));
 const datadog_1 = require("./datadog");
-function getBatchRuns(magicpod_api_token, magicpod_organization_name, magicpod_project_name, magicpod_record_count) {
+function getBatchRuns(inputs) {
     return __awaiter(this, void 0, void 0, function* () {
+        // Load insputs
+        const magicpod_api_token = inputs.magicpod_api_token;
+        const magicpod_organization_name = inputs.magicpod_organization_name;
+        const magicpod_project_name = inputs.magicpod_project_name;
+        const magicpod_record_count = inputs.magicpod_record_count;
         const url = `https://app.magicpod.com/api/v1.0/${magicpod_organization_name}/${magicpod_project_name}/batch-runs/?count=${magicpod_record_count}`;
         const headers = {
             accept: 'application/json',
@@ -33,20 +38,49 @@ function getBatchRuns(magicpod_api_token, magicpod_organization_name, magicpod_p
     });
 }
 exports.getBatchRuns = getBatchRuns;
-function processBatchRunsData(batchRunsData) {
+function processBatchRunsData(batchRunsData, inputs) {
     const organization_name = batchRunsData.organization_name;
     const project_name = batchRunsData.project_name;
     batchRunsData.batch_runs.forEach((batchRun, index) => {
         const durationSeconds = calculateTimeDifferenceSecond(batchRun.started_at, batchRun.finished_at);
         const batch_run_number = batchRun.batch_run_number;
+        (() => __awaiter(this, void 0, void 0, function* () {
+            const data = yield getBatchRun(inputs, batch_run_number);
+            if (data) {
+                processBatchRunData(data);
+            }
+            else {
+                console.log('Error occurred, no data received');
+            }
+        }))();
         const test_setting_name = batchRun.test_setting_name;
         const status = batchRun.status;
         const finished_at = batchRun.finished_at;
         const timestampSeconds = getUnixTimestampSeconds(finished_at);
-        (0, datadog_1.submitMetircs)(timestampSeconds, durationSeconds, batch_run_number, test_setting_name, status, organization_name, project_name);
+        (0, datadog_1.submitBatchRunsMetircs)(timestampSeconds, durationSeconds, batch_run_number, test_setting_name, status, organization_name, project_name);
     });
 }
 exports.processBatchRunsData = processBatchRunsData;
+function processBatchRunData(batchRunData) {
+    const organization_name = batchRunData.organization_name;
+    const project_name = batchRunData.project_name;
+    const batch_run_number = batchRunData.batch_run_number;
+    const test_setting_name = batchRunData.test_setting_name;
+    batchRunData.test_cases.details.forEach((details, index) => {
+        const pattern_name = details.pattern_name;
+        details.results.forEach((results, index) => {
+            const durationSeconds = calculateTimeDifferenceSecond(results.started_at, results.finished_at);
+            const finished_at = results.finished_at;
+            const timestampSeconds = getUnixTimestampSeconds(finished_at);
+            // const pattern_name = batchRunData.test_cases.details
+            const status = results.status;
+            const order = results.order;
+            const number = results.number;
+            (0, datadog_1.submitBatchRunMetircs)(timestampSeconds, durationSeconds, batch_run_number, test_setting_name, status, organization_name, project_name, pattern_name, order, number);
+        });
+    });
+}
+exports.processBatchRunData = processBatchRunData;
 function getUnixTimestampSeconds(dateString) {
     const dateObject = new Date(dateString);
     const unixTimestamp = dateObject.getTime();
@@ -64,3 +98,25 @@ function isStatusRunning(status) {
     return status == 'running' ? true : false;
 }
 exports.isStatusRunning = isStatusRunning;
+function getBatchRun(inputs, batch_run_number) {
+    return __awaiter(this, void 0, void 0, function* () {
+        // Load insputs
+        const magicpod_api_token = inputs.magicpod_api_token;
+        const magicpod_organization_name = inputs.magicpod_organization_name;
+        const magicpod_project_name = inputs.magicpod_project_name;
+        const url = `https://app.magicpod.com/api/v1.0/${magicpod_organization_name}/${magicpod_project_name}/batch-run/${batch_run_number}/`;
+        const headers = {
+            accept: 'application/json',
+            Authorization: `Token ${magicpod_api_token}`
+        };
+        try {
+            const response = yield axios_1.default.get(url, { headers });
+            return response.data;
+        }
+        catch (error) {
+            console.error(`Error: ${error}`);
+            return null;
+        }
+    });
+}
+exports.getBatchRun = getBatchRun;
