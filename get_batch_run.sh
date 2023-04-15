8cb77f4c61f791f7b35fb01e9a956bb2af7c7fc6
kondo takeshi <chaspy@users.noreply.github.com>
2023-04-15 12:06:02 +0900

    fix script

---
+++ get_batch_run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -eux
+BASEURL="https://app.magicpod.com/api"
+# ORGANIZATION_NAME=$ORGANIZATION_NAME
+# PROJECT_NAME=$PROJECT_NAME
+BATRH_RUN_NUMBER="1047"
+BATCH_RUNS_PATH="/v1.0/${ORGANIZATION_NAME}/${PROJECT_NAME}/batch-run/${BATRH_RUN_NUMBER}/"
+
+curl -s -X 'GET' \
+  "${BASEURL}${BATCH_RUNS_PATH}" \
+  -H 'accept: application/json' \
+  -H "Authorization: Token $MAGICPOD_API_TOKEN"
