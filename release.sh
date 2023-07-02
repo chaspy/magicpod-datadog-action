5a4a5666c43590db141623a5d73b62fc03a0e8c9
kondo takeshi <chaspy@users.noreply.github.com>
2023-07-02 17:38:33 +0900

    Get renovate commit

--- release.sh
+++ release.sh
@@ -32,14 +32,15 @@ git push origin "${new_tag}"
 
 # Get all merge commits between the latest tag and the current branch
 merge_commits=$(git log --merges --pretty=format:"%s" "${latest_tag}"..main)
-echo "${merge_commits}"
+renovate_commits=$(git log --oneline "${latest_tag}"..main | grep 'Update dependency')
+release_note_body="${merge_commits}\n${renovate_commits}"
 
 # Create a release using GitHub's API
 repo_url="https://api.github.com/repos/chaspy/magicpod-datadog-action/releases"
 response=$(curl -s -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: token $GITHUB_TOKEN" \
-  -d "{\"tag_name\":\"$new_tag\",\"name\":\"Release $new_tag\",\"body\":\"${merge_commits}\"}" \
+  -d "{\"tag_name\":\"$new_tag\",\"name\":\"Release $new_tag\",\"body\":\"${release_note_body}\"}" \
   $repo_url)
 
 # Check if the release was created successfully
